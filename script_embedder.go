e0668df06df3aa25c950be845fa22608ae75f101
_ <isentropicltd@protonmail.com>
2025-06-24 16:36:05 +0100

    update script

--- script_embedder.go
+++ script_embedder.go
@@ -1,7 +1,10 @@
 package simplebashscript
 
 import (
+	"bufio"
 	"embed"
+	"fmt"
+	"log"
 	"os/exec"
 )
 
@@ -9,5 +12,36 @@ import (
 var Script embed.FS
 
 func CallScript() {
-	exec.Command("./process.sh")
+	fmt.Println("calling script")
+	cmd := exec.Command("./process.sh")
+	stdOutPipe, _ := cmd.StdoutPipe()
+	stdErrPipe, _ := cmd.StderrPipe()
+	stdOutScanner := bufio.NewScanner(stdOutPipe)
+	stdErrScanner := bufio.NewScanner(stdErrPipe)
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	//Print the lines returned by the script to stdout as they are received
+	go func() {
+		for stdOutScanner.Scan() {
+			fmt.Println(stdOutScanner.Text())
+		}
+	}()
+
+	//Print the lines returned by the script to stderr as they are received
+	go func() {
+		for stdErrScanner.Scan() {
+			fmt.Println(stdErrScanner.Text())
+		}
+	}()
+
+	//this captures exit errors returned by the bash script
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(err)
+	}
+
+	//this captures an error in the Scanner object
+	fmt.Printf("Encountered the following errors: %v\n", stdOutScanner.Err())
 }
